381c6ab2a87dfd335e7eb85c9318280e11f9b186
MTEXX <MTEXX@users.noreply.github.com>
2021-03-18 16:13:43 +0100

    Fixes #19
    Fixes exception when user field is not set for example when seeding initial data.

---
+++ migrations/2021_03_18_160750_make_user_nullable.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakeUserNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('versions', function (Blueprint $table) {
+            $table->unsignedBigInteger(config('versionable.user_foreign_key', 'user_id'))->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('versions', function (Blueprint $table) {
+            $table->unsignedBigInteger(config('versionable.user_foreign_key', 'user_id'))->nullable(false)->change();
+        });
+    }
+}
